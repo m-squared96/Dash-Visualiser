4b2448fe38eeabe5c01c4132198c75126ab3b2e5
m-squared96 <michael.moore34@mail.dcu.ie>
2018-09-08 21:21:13 +0100

    Added some project files - incomplete

---
+++ static_stock.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import datetime
+
+import pandas_datareader.data as web
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+stock = 'TSLA'
+start = datetime.datetime(2015,1,1)
+end = datetime.datetime.now()
+df = web.DataReader(stock, 'morningstar', start, end)
+df.reset_index(inplace=True)
+df.set_index('Date', inplace=True)
+df = df.drop('Symbol', axis=1)
+
+app.layout = html.Div(children=[
+    html.H1(children='Static Dash Stock Graph'),
+    html.Div(children='''
+        Making a static stock graph
+    '''),
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data':[
+                {'x':df.index,'y':df.Close,'type':'line','name':stock},
+            ],
+            'layout':{
+                'title':stock
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
